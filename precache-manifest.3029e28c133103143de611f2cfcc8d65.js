d954c1db130a70ec74b7643e11575d6f9525271e
Felipe F Rocha <felipefonsecarocha@gmail.com>
2019-06-24 00:31:37 -0300

    Updates

---
+++ precache-manifest.3029e28c133103143de611f2cfcc8d65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722a41f332266f9a5d5cc44e481c7c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2b2fd032cb42709c1bc",
+    "url": "/static/css/main.a5046cd9.chunk.css"
+  },
+  {
+    "revision": "212917952ea33a4210c9",
+    "url": "/static/js/2.a7fea859.chunk.js"
+  },
+  {
+    "revision": "a2b2fd032cb42709c1bc",
+    "url": "/static/js/main.ccf29cb7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
